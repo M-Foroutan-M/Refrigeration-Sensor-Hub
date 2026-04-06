3d3f6fdbe4bc0511af053149f43f78a7ddb77bf4
MFM <162095614+Legionnnnn@users.noreply.github.com>
2026-04-06 23:03:00 +0100

    Update sht31_sensor.py

--- src/sensors/sht31_sensor.py
+++ src/sensors/sht31_sensor.py
@@ -1,16 +1,19 @@
 from typing import Dict, Optional
-
 import board
-from adafruit_sht31d import SHT31D
+import adafruit_sht31d
 
 
 class SHT31Sensor:
-    def __init__(self):
+    def __init__(self, i2c_address: int = 0x44):
+        self.i2c_address = i2c_address
         self.sensor = None
+        self.i2c = None
 
     def initialize(self) -> None:
-        i2c = board.I2C()
-        self.sensor = SHT31D(i2c)
+        if self.i2c is None:
+            self.i2c = board.I2C()
+
+        self.sensor = adafruit_sht31d.SHT31D(self.i2c, address=self.i2c_address)
 
     def read(self) -> Dict[str, Optional[float]]:
         if self.sensor is None:
